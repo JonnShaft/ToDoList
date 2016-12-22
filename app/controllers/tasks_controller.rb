fe7b9fedc94fa03fcc44a1ccc20a72e6439bde5c
JonnShaft <akj824@gmail.com>
2016-12-22 09:21:23 -0700

    Add task creation in API

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,9 +9,14 @@ class TasksController < ApplicationController
 		render json: task #Response should be the JSON representation of the item updated.
 	end
 
+	def create
+		task = Task.create(task_params)
+		render json: task
+	end
+
 	private
 
 	def task_params
-		params.require(:task).permit(:done)
+		params.require(:task).permit(:done, :title)
 	end
 end
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -26,4 +26,14 @@ RSpec.describe TasksController, type: :controller do
 			expect(task.done).to eq(true) #Attribute for "done" should be changed from false to true.
 		end
 	end
+
+	describe "task#create" do
+		it "should allow new tasks to be created" do
+			post :create, task: {title: "Fix things"}	#POST request on the create action.
+			expect(response).to have_http_status(:success)	#POST request is expected to be successful.
+			response_value = ActiveSupport::JSON.decode(@response.body)	#Response shold be parsed as JSON and ensure the title has been populated.
+			expect(response_value['title']).to eq("Fix things")
+			expect(Task.last.title).to eq("Fix things")	#Last item should be stored in the DB.
+		end
+	end
 end
