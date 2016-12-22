a40165696ec9acb13f04ed43b20e044dfa3f957e
JonnShaft <akj824@gmail.com>
2016-12-21 20:10:40 -0700

    Incorporate functionality to update tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,16 @@ class TasksController < ApplicationController
 	def index
 		render json: Task.all
 	end
+
+	def update
+		task = Task.find(params[:id])	#Load the correct item from the DB.	
+		task.update_attributes(task_params)	#Values should be updated with the values passed through the HTTP request.
+		render json: task #Response should be the JSON representation of the item updated.
+	end
+
+	private
+
+	def task_params
+		params.require(:task).permit(:done)
+	end
 end
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -3,12 +3,22 @@ require 'rails_helper'
 RSpec.describe TasksController, type: :controller do
 	describe "tasks#index" do
 		it "should list the tasks in the database" do
-			task1 = FactoryGirl.create(:task)
+			task1 = FactoryGirl.create(:task)	#Create items in DB.
 			task2 = FactoryGirl.create(:task)
-			get :index
-			expect(response).to have_http_status :success
-			response_value = ActiveSupport::JSON.decode(@response.body)
-			expect(response_value.count).to eq(2)
+			get :index	#Trigger the index action.
+			expect(response).to have_http_status :success  #HTTP request should be successful.
+			response_value = ActiveSupport::JSON.decode(@response.body)	#Access the actual response from the app.
+			expect(response_value.count).to eq(2)	#Text in JSON should be produced.
+		end
+	end
+
+	describe "tasks#update" do
+		it "should allow tasks to be marked as done" do
+			task = FactoryGirl.create(:task, done: false)	#Create item in DB.
+			put :update, id: task.id, task: { done: true } #PUT request should trigger update, done value should be updated to "true" to toggle item's completion status.
+			expect(response).to have_http_status(:success) #HTTP request should produce a successful response.
+			task.reload #Reload task from DB.
+			expect(task.done).to eq(true) #Attribute for "done" should be changed from false to true.
 		end
 	end
 end
